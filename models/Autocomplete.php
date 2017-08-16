334f571ff2c2e829e52794fcae69dce06b953355
matt murphy <mattyhead@gmail.com>
2017-08-16 15:43:18 -0400

    ...

--- models/Autocomplete.php
+++ models/Autocomplete.php
@@ -28,42 +28,39 @@ class Autocomplete {
 
 		$sql = ' SELECT DISTINCT ${self::fields} FROM ${self::table} WHERE ${self::criteria} ORDER BY street_name LIMIT 0, 10 ';
 
-		$sql = 'SELECT * FROM stuff';
 		$stmt = $this->core->dbh->prepare( $sql );
 
 		if ( $stmt->execute( $this->params ) ) {
-			$r = $stmt->fetchAll( PDO::FETCH_ASSOC );
-		} else {
-			$r = 0;
+			return $stmt->fetchAll( PDO::FETCH_ASSOC );
 		}
-		return $r;
+
+		return false;
 	}
 
 	protected function setup() {
 		$this->callback = urldecode( $_GET['callback'] );
-		$address = urldecode( $_GET['address'] )
-		$parts    = explode( ' ', $address );
+		$address = urldecode( $_GET['address'] );
+		$parts   = explode( ' ', $address );
 
-		$fields   = 'prefix_dir, TRIM(LEADING \'0\' FROM street_name) as street, type_dir, ';
-		$criteria = 'house_range_start <= :a1 and house_range_end >= :a2';
+		$this->fields   = 'prefix_dir, TRIM(LEADING \'0\' FROM street_name) as street, type_dir, ';
+		$this->criteria = 'house_range_start <= :a1 and house_range_end >= :a2';
 
 		switch ( count( $parts ) ) {
-			case 0:
-				break;
 			case 1:
-				$criteria .= '';
+				$this->criteria .= '';
 				$this->params = array(
 					':a1' => $parts[0],
 					':a2' => $parts[0],
 				);
 				break;
 			case 2:
-				$criteria .= ' and street_name LIKE :a3 ';
+				$this->criteria .= ' and street_name LIKE :a3 ';
 				$this->params = array(
 					':a1' => $parts[0],
 					':a2' => $parts[0],
 					':a3' => $parts[1] . '%',
 				);
+				break;
 			default:
 				break;
 		}
