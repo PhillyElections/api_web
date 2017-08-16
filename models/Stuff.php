135296b4eb568f2ca2dd3940178a5467af30facf
matt murphy <mattyhead@gmail.com>
2017-08-16 14:39:04 -0400

    ...

--- models/Stuff.php
+++ models/Stuff.php
@@ -9,25 +9,25 @@ class Stuff {
 	protected $core;
 
 	function __construct() {
-		//$this->core = \lib\Core::getInstance();
+		$this->core = \lib\Core::getInstance();
 	}
-	
+
 	// Get all stuff
 	public function getAllStuff() {
-		$r = array();		
+		$r = array();
 
-		$sql = "SELECT * FROM stuff";
-		$stmt = $this->core->dbh->prepare($sql);		
+		$sql = 'SELECT * FROM stuff';
+		$stmt = $this->core->dbh->prepare( $sql );
 
-		if ($stmt->execute()) {
-			$r = $stmt->fetchAll(PDO::FETCH_ASSOC);		   	
+		if ( $stmt->execute() ) {
+			$r = $stmt->fetchAll( PDO::FETCH_ASSOC );
 		} else {
 			$r = 0;
-		}		
+		}
 		return $r;
 	}
 
-    public function setStuff() {
-        return "hello world!!!";
-    }
+	public function setStuff() {
+		return 'hello world!!!';
+	}
 }
