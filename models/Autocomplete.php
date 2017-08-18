9331147a712f1de7d0db28bd91b844030806c0f1
matt murphy <mattyhead@gmail.com>
2017-08-18 16:00:31 -0400

    ...

--- models/Autocomplete.php
+++ models/Autocomplete.php
@@ -51,6 +51,7 @@ class Autocomplete
         try {
             $results = $stmt->execute($this->params);
             $json = $this->callback . '(' . json_encode($results) . ')';
+            d($stmt);
         } catch (PDOException $e) {
             d($this, $e->getMessage(), $e, $stmt);
         }
