b85fe6cfcc4019eb98b5049f5f4acfe62c61f733
matt murphy <mattyhead@gmail.com>
2017-08-28 17:15:25 -0400

    ...

--- routers/admin.router.php
+++ routers/admin.router.php
@@ -6,7 +6,7 @@ $app->get(
     function () use ($app) {
         $admin = new models\Admin();
         $content = $admin->getContent();
-
+        d($app);
         // $content is segmented into title/header/body
         $app->render(
             'admin.html',
