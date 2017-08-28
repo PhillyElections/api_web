8cd4a824f1b4134d8370b92193fe104c21ce6f12
matt murphy <mattyhead@gmail.com>
2017-08-28 10:35:49 -0400

    step two of moving from scripts to web admin (step 1 being htaccess)

---
+++ routers/admin.router.php
@@ -0,0 +1,20 @@
+<?php
+
+// GET index route
+$app->get('/admin', function () use ($app) {
+    $admin = new models\Admin();
+
+    $content = $admin->getContent();
+    // $content is segmented into title/header/body
+    $app->render(
+        'admin.html',
+        $content
+    );
+});
+
+// POST index route
+$app->post('/admin', function () use ($app) {
+    $admin = new models\Admin();
+
+    $content = $admin->process();
+});
