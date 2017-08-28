a692b8b3ec4e5e9fceaec6c52f9b43f001f28523
matt murphy <mattyhead@gmail.com>
2017-08-27 21:22:27 -0400

    adding scripts

--- scripts/refresh.table.php
+++ scripts/refresh.table.php
@@ -1,6 +1,10 @@
 <?php
+
 require '../vendor/autoload.php';
-dd('kint loaded');
+
+$core = \lib\Core::getInstance();
+
+dd($core, 'kint loaded');
 // Parameters
 if ($argv[1]) {
     $file = $argv[1];
@@ -22,44 +26,44 @@ if (! $fp = fopen($file, 'r')) {
 
 $frow = fgetcsv($fp);
 
-$ccount = 0;
-foreach ($frow as $column) {
-    $ccount++;
-    if ($columns) {
-        $columns .= ', ';
-    }
-    $columns .= "$column varchar(250)";
-}
+$drop = 'DROP TABLE IF EXISTS `block_range`';
 
 $create = "
-DROP TABLE IF EXISTS `block_range`;
 CREATE TABLE `block_range` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `house_range_start` int(6) UNSIGNED NOT NULL DEFAULT '0',
-  `house_range_end` int(6) UNSIGNED NOT NULL DEFAULT '0',
-  `prefix_dir` char(1) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `street_name` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `type_dir` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `zip_code` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `block_id` int(11) DEFAULT NULL,
-  `precinct_split` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated` datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `id` INT(11) UNSIGNED NOT NULL,
+  `oeb` ENUM('O','E','B') NOT NULL DEFAULT 'B',
+  `range_start` INT(6) UNSIGNED NOT NULL DEFAULT '0',
+  `range_end` INT(6) UNSIGNED NOT NULL DEFAULT '0',
+  `prefix_dir` CHAR(1) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `street_name` VARCHAR(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `suffix_dir` CHAR(1) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `suffix_type` VARCHAR(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `zip_code` VARCHAR(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `city` VARCHAR(15) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `zip` INT(5) UNSIGNED ZEROFILL NOT NULL DEFAULT '0',
+  `block_id` INT(11) DEFAULT NULL,
+  `usage` CHAR(1) NOT NULL DEFAULT '',
+  `status` TINYINT(1) NOT NULL DEFAULT '0',
+  `precinct` INT(4) UNSIGNED ZEROFILL NOT NULL DEFAULT '0',
+  `created` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=Aria DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 ";
 
-$qry = $dbcon->prepare($create);
+$qry = $core->dbh->prepare($delete);
+$qry->execute();
+
+$qry = $core->dbh->prepare($create);
 $qry->execute();
 
 /********************************************************************************/
 // Import the data into the newly created table.
 
-import = <<<_IMPORT
+$import = <<<_IMPORT
 mysql --user=$user --password=$pass $dbName --execute="LOAD DATA LOCAL INFILE '$dest' INTO TABLE jos_rt_imports FIELDS TERMINATED BY '$delim' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' $ignore ($sFields) SET $lField = TRIM(TRAILING '\r' FROM @var);"
 _IMPORT;
 
 system($import);
-
 
 $qry = $dbcon->prepare("load data infile '$file' into table $table fields terminated by ',' ignore 1 lines");
 $qry->execute();
