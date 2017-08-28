a975ecff07e8a8d9f63ef4b1aa8e15f0b85e00c5
matt murphy <mattyhead@gmail.com>
2017-08-28 12:37:00 -0400

    ...

--- routers/admin.router.php
+++ routers/admin.router.php
@@ -1,17 +1,20 @@
 <?php
 
 // GET index route
-$app->get('/admin', function () use ($app) {
-    $admin = new models\Admin();
+$app->get(
+    '/admin',
+    function () use ($app) {
+        $admin = new models\Admin();
 
-    $content = $admin->getContent();
-    d($content);
-    // $content is segmented into title/header/body
-    $app->render(
-        'admin.html',
-        $content
-    );
-});
+        $content = $admin->getContent();
+        d($content);
+        // $content is segmented into title/header/body
+        $app->render(
+            'admin.html',
+            $content
+        );
+    }
+);
 
 // POST index route
 $app->post('/admin', function () use ($app) {
