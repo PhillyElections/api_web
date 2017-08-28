0419060d6f17d51249b31a55fabe0ea7d9012c27
matt murphy <mattyhead@gmail.com>
2017-08-28 12:05:34 -0400

    naked admin model, moved titlecase to Utils class

---
+++ models/Admin.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Admin model.
+ *
+ * API admin model
+ *
+ * @link       https://www.philadelphiavotes.com
+ *
+ * @package    api_web
+ * @subpackage api_web/models
+ */
+
+namespace models;
+
+/**
+ * Admin class.
+ *
+ * @link       https://www.philadelphiavotes.com
+ *
+ * @package    api_web
+ * @subpackage api_web/models
+ */
+class Admin
+{
+    protected $core;
+
+    /**
+     * Constructor: get core, call setup to process request.
+     */
+    public function __construct()
+    {
+        $this->core = \lib\Core::getInstance();
+
+        // process _REQUEST.
+        $this->setup();
+    }
+}
