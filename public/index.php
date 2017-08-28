13489ed86a40b1031210539aeb0975d5e29b474b
matt murphy <mattyhead@gmail.com>
2017-08-28 17:26:23 -0400

    ...

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-/*// Get Env variable to automatically include environment config
+// Get Env variable to automatically include environment config
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'local'));
 
 // show errors when working on local
@@ -27,32 +27,4 @@ foreach ($routers as $router) {
     require $router;
 }
 
-$app->run();
-*/
-
-// Create Slim app
-$app = new \Slim\App();
-
-// Fetch DI Container
-$container = $app->getContainer();
-
-// Register Twig View helper
-$container['view'] = function ($c) {
-    $view = new \Slim\Views\Twig('../templates');
-
-    // Instantiate and add Slim specific extension
-    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
-
-    return $view;
-};
-
-// Define named route
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $this->view->render($response, 'profile.html', [
-        'name' => $args['name']
-    ]);
-})->setName('profile');
-
-// Run app
 $app->run();
