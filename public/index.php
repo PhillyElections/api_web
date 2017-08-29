72496a1a09199a3564160abc335c8cc0bd70e345
matt murphy <mattyhead@gmail.com>
2017-08-29 15:08:22 -0400

    ...

--- public/index.php
+++ public/index.php
@@ -1,56 +1,15 @@
 <?php
-/*
-// Get Env variable to automatically include environment config
-defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'local'));
 
-// show errors when working on local
-if (APPLICATION_ENV === 'local') {
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-}
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 require '../vendor/autoload.php';
-require '../configs/'.strtolower(APPLICATION_ENV).'.config.php';
 
-// Setup custom Twig view
-$twigView = new \Slim\Views\Twig();
-
-$app = new \Slim\Slim(array(
-    'debug' => true,
-    'view' => $twigView,
-    'templates.path' => '../templates/',
-));
-
-// Automatically load router files
-$routers = glob('../routers/*.router.php');
-foreach ($routers as $router) {
-    require $router;
-}
-
-$app->run();
-*/
-
-// Create Slim app
 $app = new \Slim\App();
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
 
-// Fetch DI Container
-$container = $app->getContainer();
-
-// Register Twig View helper
-$container['view'] = function ($c) {
-    $view = new \Slim\Views\Twig('../templates', [
-        'cache' => '../cache'
-    ]);
-
-    return $view;
-};
-exit;
-// Define named route
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $this->view->render($response, 'profile.html', [
-        'hello' => $args['name']
-    ]);
-})->setName('profile');
-
-// Run app
+    return $response;
+});
 $app->run();
