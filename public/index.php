2de0009c27c2a9dcf64ec36daf90c567dcc6c81e
matt murphy <mattyhead@gmail.com>
2017-08-29 14:25:02 -0400

    ...

--- public/index.php
+++ public/index.php
@@ -30,16 +30,31 @@ foreach ($routers as $router) {
 $app->run();
 */
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-
-require '../vendor/autoload.php';
-
+// Create Slim app
 $app = new \Slim\App();
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
 
-    return $response;
-});
+// Fetch DI Container
+$container = $app->getContainer();
+
+// Register Twig View helper
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('path/to/templates', [
+        'cache' => 'path/to/cache'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
+
+// Define named route
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->view->render($response, 'index.html', [
+        'hello' => $args['name']
+    ]);
+})->setName('profile');
+
+// Run app
 $app->run();
