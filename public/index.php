342a74faceca73b596d73b515ac6f09fa18a9cd6
matt murphy <mattyhead@gmail.com>
2017-08-29 14:06:37 -0400

    ...

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 // Get Env variable to automatically include environment config
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'local'));
 
@@ -27,4 +27,19 @@ foreach ($routers as $router) {
     require $router;
 }
 
+$app->run();
+*/
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App();
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
 $app->run();
