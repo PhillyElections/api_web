2fa2bdd38da8b01093ea45adb80593e1f91e1510
matt murphy <mattyhead@gmail.com>
2017-08-29 08:44:56 -0400

    ...

--- public/index.php
+++ public/index.php
@@ -1,45 +1,30 @@
 <?php
 
-/*// Get Env variable to automatically include environment config
-defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'local'));
-
-// show errors when working on local
-if (APPLICATION_ENV === 'local') {
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-}
-
-require '../vendor/autoload.php';
-require '../configs/'.strtolower(APPLICATION_ENV).'.config.php';
-
-// Setup custom Twig view
-$twigView = new \Slim\Views\Twig();
-
-$app = new \Slim\Slim(array(
-    'debug' => true,
-    'view' => $twigView,
-    'templates.path' => '../templates/',
-));
-
-// Automatically load router files
-$routers = glob('../routers/*.router.php');
-foreach ($routers as $router) {
-    require $router;
-}
-
-$app->run();
-*/
-
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-
-require '../vendor/autoload.php';
-
+// Create Slim app
 $app = new \Slim\App();
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
 
-    return $response;
-});
+// Fetch DI Container
+$container = $app->getContainer();
+
+// Register Twig View helper
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('path/to/templates', [
+        'cache' => 'path/to/cache'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
+
+// Define named route
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->view->render($response, 'index.html', [
+        'hello' => $args['name']
+    ]);
+})->setName('profile');
+
+// Run app
 $app->run();
