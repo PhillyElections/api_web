dee1aa6d4f534fa2d1ed8a3bf33055d0fd31e8ed
matt murphy <mattyhead@gmail.com>
2017-08-29 13:39:42 -0400

    restoring orignal bootstrap

--- public/index.php
+++ public/index.php
@@ -1,36 +1,30 @@
 <?php
 
-// Create Slim app
-$app = new \Slim\App();
+// Get Env variable to automatically include environment config
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'local'));
 
-// Fetch DI Container
-$container = $app->getContainer();
+// show errors when working on local
+if (APPLICATION_ENV === 'local') {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
 
-// Register Twig View helper
-$container['view'] = function ($c) {
-    $view = new \Slim\Views\Twig('app/templates', [
-        'cache' => 'app/cache'
-    ]);
+require '../vendor/autoload.php';
+require '../configs/'.strtolower(APPLICATION_ENV).'.config.php';
 
-    // Instantiate and add Slim specific extension
-    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
+// Setup custom Twig view
+$twigView = new \Slim\Views\Twig();
 
-    return $view;
-};
+$app = new \Slim\Slim(array(
+    'debug' => true,
+    'view' => $twigView,
+    'templates.path' => '../templates/',
+));
 
 // Automatically load router files
-/*$routers = glob('../routers/*.router.php');
+$routers = glob('../routers/*.router.php');
 foreach ($routers as $router) {
     require $router;
 }
-*/
-// Define named route
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $this->view->render($response, 'index.html', [
-       'hello' => $args['name']
-   ]);
-})->setName('profile');
 
-// Run app
 $app->run();
