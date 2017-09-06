71ef1ab436c2b3af10955ced5c89d5012c58a8f6
matt murphy <mattyhead@gmail.com>
2017-09-06 09:58:07 -0400

    ...

--- routers/autocomplete.router.php
+++ routers/autocomplete.router.php
@@ -4,6 +4,8 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app->get('/autocomplete/{address}', function (Request $request, Response $response) {
+    d('haven\'t returned yet', $request->getHeader('host'));
+    exit;
     if (in_array($request->getHeader('host')[0], array('apis.philadelphiavotes.com', 'www.philadelphiavotes.com', 'philadelphiavotes.com'))) {
         $address = $request->getAttribute('address');
         $autocomplete = new models\Autocomplete($address);
@@ -12,8 +14,6 @@ $app->get('/autocomplete/{address}', function (Request $request, Response $respo
 
         return $response;
     }
-    d('haven\'t returned yet');
-    exit;
 
     return $response->withStatus('401', 'Unauthorized');
 });
