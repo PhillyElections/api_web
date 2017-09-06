dcf730162672ff749f43979239edffbee0f42d10
matt murphy <mattyhead@gmail.com>
2017-09-06 09:59:56 -0400

    ...

--- routers/autocomplete.router.php
+++ routers/autocomplete.router.php
@@ -4,9 +4,10 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app->get('/autocomplete/{address}', function (Request $request, Response $response) {
-    d('haven\'t returned yet', $request->getHeader('host'), in_array($request->getHeader('host')[0], array('apis.philadelphiavotes.com', 'www.philadelphiavotes.com', 'philadelphiavotes.com')));
-    exit;
     if (in_array($request->getHeader('host')[0], array('apis.philadelphiavotes.com', 'www.philadelphiavotes.com', 'philadelphiavotes.com'))) {
+        d('haven\'t returned yet', $request->getHeader('host'), in_array($request->getHeader('host')[0], array('apis.philadelphiavotes.com', 'www.philadelphiavotes.com', 'philadelphiavotes.com')));
+        exit;
+
         $address = $request->getAttribute('address');
         $autocomplete = new models\Autocomplete($address);
 
