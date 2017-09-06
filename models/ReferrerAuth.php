5258f411805cdfce803db28a3706d12817fed027
matt murphy <mattyhead@gmail.com>
2017-09-06 11:43:07 -0400

    ...

--- models/ReferrerAuth.php
+++ models/ReferrerAuth.php
@@ -55,7 +55,7 @@ class ReferrerAuth
         $stmt->bindParam(':a1', $referrer, PDO::PARAM_STR);
 
         if ($stmt->execute()) {
-            $value = $stmt->fetch(PDO::FETCH_ASSOC);
+            $value = $stmt->fetchColumn(PDO::FETCH_ASSOC);
             d($value);
             exit;
         }
