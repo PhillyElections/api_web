42b530e2e44b0d0dbb053f4ed4498ee40b18cc93
matt murphy <mattyhead@gmail.com>
2017-09-06 07:45:22 -0400

    ...

--- routers/index.router.php
+++ routers/index.router.php
@@ -2,6 +2,8 @@
 
 // GET index route
 $app->get('/', function (Request $request, Response $response) {
+    d($this, $request, $response, 1);
+    exit;
     $response->getBody()->write('Move allong... Nothing to see here.');
 
     return $response;
