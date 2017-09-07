05abc0650311dde0c1c07d9d8e832b4c26bd7e14
matt murphy <mattyhead@gmail.com>
2017-09-07 09:50:50 -0400

    ...

--- routers/index.router.php
+++ routers/index.router.php
@@ -5,7 +5,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 // GET index route
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('Move allong... Nothing to see here.');
+    $response->getBody()->write('Move along... Nothing to see here.');
 
     return $response;
 });
