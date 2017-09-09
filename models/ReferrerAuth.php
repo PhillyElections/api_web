bca0ff21d7d43120e1bd7e6727696f8ae4df74fb
Matthew Murphy <mattyhead@gmail.com>
2017-09-09 14:46:39 -0400

    authenticate by referrer per api

--- models/ReferrerAuth.php
+++ models/ReferrerAuth.php
@@ -26,6 +26,7 @@ class ReferrerAuth
 {
     protected $core;
     protected $request;
+    protected $api;
     protected $table = 'referrers';
 
     /**
@@ -34,30 +35,48 @@ class ReferrerAuth
      * @param mixed $address
      * @param mixed $request
      */
-    public function __construct($request)
+    public function __construct($request, $api)
     {
         $this->core = \lib\Core::getInstance();
+        $this->api = &$api;
         $this->request = &$request;
     }
 
     /**
-     * Fetch results based on setup().
+     * authenticate based on __construc() values.
      *
      * @return     boolean  A JsonP function.
      */
     public function authenticate()
     {
-        $referrer = $this->request->getHeader('host')[0];
-        $value = false;
-        $sql = ' SELECT COUNT(name) FROM ' . $this->table . ' WHERE name = :a1 ';
+        $sql = ' SELECT COUNT(name) FROM ' . $this->table . ' WHERE api = :api ';
 
         $stmt = $this->core->dbh->prepare($sql);
-        $stmt->bindParam(':a1', $referrer, PDO::PARAM_STR);
+        $stmt->bindParam(':api', $this->api, PDO::PARAM_STR);
 
         if ($stmt->execute()) {
-            $value = (int) $stmt->fetchColumn() >= 1 ? true : false;
+            // if we have no rows restricting this API when we fetch 0
+            if ((int) $stmt->fetchColumn() == 0) {
+                // then we can go ahead and process
+                return true;
+            }
         }
 
-        return $value;
+        // Ok, we're still here, so we need to reinitialize for the real check.
+        $referrer = $this->request->getHeader('host')[0];
+        $sql = ' SELECT COUNT(name) FROM ' . $this->table . ' WHERE api = :api name = :referrer ';
+
+        $stmt = $this->core->dbh->prepare($sql);
+        $stmt->bindParam(':api', $this->api, PDO::PARAM_STR);
+        $stmt->bindParam(':referrer', $referrer, PDO::PARAM_STR);
+
+        if ($stmt->execute()) {
+            if ((int) $stmt->fetchColumn() >= 1) {
+                // then we can go ahead and process
+                return true;
+            }
+        }
+
+        return false;
     }
 }
