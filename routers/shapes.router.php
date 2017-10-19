7be32d1302f5549a2bb1e7c037d15c3a3926840e
matt murphy <mattyhead@gmail.com>
2017-10-19 09:12:18 -0400

    let's just get up that empty route

---
+++ routers/shapes.router.php
@@ -0,0 +1,42 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+$app->get('/shapes/}', function (Request $request, Response $response) {
+    $referrer = $this->request->getHeader('host')[0];
+    $referrerAuth = new models\ReferrerAuth($referrer, 'shapes');
+
+    if ($referrerAuth->authenticate()) {
+        $response->getBody()->write(
+            '<h1>Available Shape Services:</h1>' .
+            '<ul>' .
+            '  <li><a href="/shapes/us_congress/">US Congressional</a> (/shapes/us_congress)</li>' .
+            '</ul>'
+        );
+
+        return $response->withHeader('Content-Type', 'text/html')->withHeader('Access-Control-Allow-Origin', '*');
+    }
+
+    $response->getBody()->write('<h1>401: Unauthorized</h1>');
+
+    return $response->withStatus(401);
+});
+
+$app->get('/shapes/us_congress/{district}', function (Request $request, Response $response) {
+    $referrer = $this->request->getHeader('host')[0];
+    $referrerAuth = new models\ReferrerAuth($referrer, 'shapes');
+
+    if ($referrerAuth->authenticate()) {
+        $district = $request->getAttribute('district');
+
+        $model = new models\UsCongress($district);
+        $response->getBody()->write($model->fetch());
+
+        return $response->withHeader('Content-Type', 'application/json')->withHeader('Access-Control-Allow-Origin', '*');
+    }
+
+    $response->getBody()->write('<h1>401: Unauthorized</h1>');
+
+    return $response->withStatus(401);
+});
