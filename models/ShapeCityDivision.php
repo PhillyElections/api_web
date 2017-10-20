f927acffd470ec97d53087be116bdc311015647a
matt murphy <mattyhead@gmail.com>
2017-10-20 13:04:52 -0400

    ...

--- models/ShapeCityDivision.php
+++ models/ShapeCityDivision.php
@@ -70,7 +70,7 @@ class ShapeCityDivision
                     $features[0]['attributes']['queried'] = $this->queried;
 
                     $features[0]['geometry'] = $features[0]['geometry']['coordinates'] = [];
-                    $features[0]['geometry']['coordinates'][0] = $data[0]['SHAPE'];//$utils->polygonString2Array($data[0]['SHAPE']);
+                    $features[0]['geometry']['coordinates'][0] = $utils->polygonString2Array($data[0]['SHAPE']);
                     $status = 'success';
                 }
             }
