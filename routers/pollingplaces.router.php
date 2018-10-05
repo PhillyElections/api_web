d7b8067934321fccd136b0f746604bfae0ecdcaf
matthew murphy <mattyhead@gmail.com>
2018-10-05 15:22:01 -0400

    ...

--- routers/pollingplaces.router.php
+++ routers/pollingplaces.router.php
@@ -3,7 +3,7 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-$app->get('/pollingplaces/fulllist/', function (Request $request, Response $response) {
+/*$app->get('/pollingplaces/fulllist/', function (Request $request, Response $response) {
     $response->getBody()->write("We'll show a list of all polling places.");
 
     return $response;
@@ -14,9 +14,10 @@ $app->get('/pollingplaces/wardlist/{ward}', function (Request $request, Response
     $response->getBody()->write("We'll show a list of polling places for the ward: $ward.");
 
     return $response;
-});
+});*/
 
 $app->get('/pollingplaces/{precinct}', function (Request $request, Response $response) {
+
     $precinct = $request->getAttribute('precinct');
     $pollingplaces = new models\Pollingplaces($precinct);
     $callback = $request->getParam('callback');    
