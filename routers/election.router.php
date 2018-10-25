7d9e33f797b409e4e38ea9ae6e31b5ddca071225
matthew murphy <mattyhead@gmail.com>
2018-10-25 08:38:55 -0400

    ...

--- routers/election.router.php
+++ routers/election.router.php
@@ -16,3 +16,18 @@ $app->get('/election', function (Request $request, Response $response) {
 
     return $response->withHeader('Content-Type', ($callback? 'application/javascript': 'application/json'))->withHeader('Access-Control-Allow-Origin', '*');
 });
+
+$app->get('/election/{queried}', function (Request $request, Response $response) {
+    // exclude auth
+/*    $referrer = $this->request->getHeader('host')[0];
+    $referrerAuth = new models\ReferrerAuth($referrer, 'indexes');
+*/
+    $callback = $request->getParam('callback'); 
+    $queried = $request->getParam('callback'); 
+
+    $model = new models\Election($queried);
+
+    $response->getBody()->write( ($callback ? $callback . '(' : '') . $model->fetch() . ($callback ? ');' : '' ) );
+
+    return $response->withHeader('Content-Type', ($callback? 'application/javascript': 'application/json'))->withHeader('Access-Control-Allow-Origin', '*');
+});
