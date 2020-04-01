07b79effca9f18855c2a5625428e71e3a1101c0d
Matthew Murphy <murphym@freelibrary.org>
2020-04-01 11:36:20 -0400

    Add override to 2020 primary...
    ....without messing up anything else.  Also corrected doc blocks.

--- models/Election.php
+++ models/Election.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * ElectedOfficials model.
+ * Election model.
  *
- * ElectedOfficials API's model
+ * Election API model
  *
  * @link       https://www.philadelphiavotes.com
  *
@@ -12,10 +12,8 @@
 
 namespace models;
 
-use PDO;
-
 /**
- * ElectedOfficials model.
+ * Election model: return the correct upcoming primary or general from any given date (YYYY-MM-DD)
  *
  * @link       https://www.philadelphiavotes.com
  *
@@ -38,8 +36,11 @@ class Election
     public function __construct($date = '')
     {
         $this->actual_request = $date;
-        if (!$date) $date = date("Y-m-d");
-        if (\DateTime::createFromFormat('Y-m-d', $date) == FALSE) {
+        if (!$date) {
+            $date = date("Y-m-d");
+        }
+
+        if (\DateTime::createFromFormat('Y-m-d', $date) == false) {
             $this->message = "Had trouble reading your date.  Preferred format is: YYYY-MM-DD.";
             return;
         }
@@ -58,7 +59,7 @@ class Election
     public function fetch()
     {
         if ($this->message) {
-            return json_encode(array('message'=>$this->message));
+            return json_encode(array('message' => $this->message));
         }
         return json_encode($this->getNextElection());
     }
@@ -67,7 +68,8 @@ class Election
      * Get next election
      *
      */
-    private function getNextElection() {
+    private function getNextElection()
+    {
 
         $year = $this->date->format("Y");
         // set general first
@@ -95,24 +97,41 @@ class Election
         return $this->getReturnArray($this->primary, 'primary');
     }
 
-    private function getReturnArray($election, $type) {
+    private function getReturnArray($election, $type)
+    {
+        // 2020 override code
+        // if the returned election date is the canonical correct date of the 2020 primary...
+        if ($election->format("Y-m-d") == '2020-04-28') {
+            // return the alternate date in the array
+            return array(
+                'election_type' => $type,
+                'election_date' => '2020-06-02',
+                'from_date' => $this->date->format("Y-m-d"),
+                'actual_request' => $this->actual_request,
+            );
+        }
+
+        // original code
         return array(
-            'election_type'=>$type, 
-            'election_date'=>$election->format("Y-m-d"), 
-            'from_date'=>$this->date->format("Y-m-d"), 
-            'actual_request'=>$this->actual_request
+            'election_type' => $type,
+            'election_date' => $election->format("Y-m-d"),
+            'from_date' => $this->date->format("Y-m-d"),
+            'actual_request' => $this->actual_request,
         );
     }
 
-    private function setPresidentialPrimary($year) {
+    private function setPresidentialPrimary($year)
+    {
         $this->primary = new \DateTime("fourth tuesday of april $year");
     }
 
-    private function setPrimary($year) {
+    private function setPrimary($year)
+    {
         $this->primary = new \DateTime("third tuesday of may $year");
     }
 
-    private function setGeneral($year) {
+    private function setGeneral($year)
+    {
         $first_monday_november = new \DateTime("first monday of november $year");
         $this->general = $first_monday_november->modify("this tuesday");
     }
